d5ae99458a343aafbcce465d6db1dc2705e70fda
Josh Arts <josha5@outlook.com>
2016-10-07 23:33:16 -0400

    Added player searching.

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,21 +2,26 @@ class PlayersController < ApplicationController
     def index
         # Sort by region.
         # Make it so you can sort by region and stats.
+        @player = Player.all
+        
         if(params[:region])
             @player = Player.where(region: params[:region])
-        elsif(params[:sortby] == "goals")
-            @player = Player.by_gpg
-        elsif(params[:sortby] == "assists")
-            @player = Player.by_apg
-        elsif(params[:sortby] == "saves")
-            @player = Player.by_sapg
-        elsif(params[:sortby] == "shots")
-            @player = Player.by_shpg
-        elsif(params[:sortby] == "points")
-            @player = Player.by_ppg
-        else
-            @player = Player.all
         end
+        
+        # Restrict to matches in certain stage.
+        
+        if(params[:sortby] == "goals")
+            @player = @player.by_gpg
+        elsif(params[:sortby] == "assists")
+            @player = @player.by_apg
+        elsif(params[:sortby] == "saves")
+            @player = @player.by_sapg
+        elsif(params[:sortby] == "shots")
+            @player = @player.by_shpg
+        elsif(params[:sortby] == "points")
+            @player = @player.by_ppg
+        end
+        
     end
     
     def show
